48a2cf2e95adc3486081829dfd456d471484d330
mr ranvijay <ranvijay.cse@outlook.com>
2020-06-03 23:31:57 +0530

    mongo db configuration for heroku

--- config/appConfig.js
+++ config/appConfig.js
@@ -4,7 +4,8 @@ appConfig.port = process.env.PORT || 3000;
 appConfig.allowedCorsOrigin = '*';
 appConfig.env = 'dev';
 appConfig.db = {
-    uri: 'mongodb://127.0.0.1:27017/avengersDbs'
+    uri: 'mongodb://ranvijay:rv144#@ds031792.mlab.com:31792/heroku_tvtkwnmt'
+    // uri: 'mongodb://127.0.0.1:27017/avengersDbs'
 };
 appConfig.apiVersion = '/api/v1';
 
